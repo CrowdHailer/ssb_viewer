0bfbf20b45bddc3d473ae743fa3d53a4a82588ac
CrowdHailer <peterhsaxton@gmail.com>
2018-05-15 22:01:36 +0100

    correctly use curve keys

--- src/secret_handshake.erl
+++ src/secret_handshake.erl
@@ -3,7 +3,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 % takes ephemeral
-hello({PublicKey, _SecretKey}, NetworkIdentifier) ->
+hello(PublicKey, NetworkIdentifier) ->
   HMAC = libsodium_crypto_auth:crypto_auth(PublicKey, NetworkIdentifier),
   <<HMAC/binary, PublicKey/binary>>.
 
@@ -15,9 +15,10 @@ verify_hello(<<HMAC:32/binary, PeerEphemeral:32/binary>>, NetworkIdentifier) ->
       {error, could_not_verify}
   end.
 
-shared_ab(PeerPublicKey, {_, <<LHS:32/binary, RHS:32/binary>>}) ->
-  libsodium_crypto_scalarmult:crypto_scalarmult(LHS, PeerPublicKey).
 
+curve25519keypair() ->
+  {PK, SK} = libsodium_crypto_sign_ed25519:keypair(),
+  {libsodium_crypto_sign_ed25519:pk_to_curve25519(PK), libsodium_crypto_sign_ed25519:sk_to_curve25519(SK)}.
 
 run_test() ->
   % From Python source
@@ -25,18 +26,31 @@ run_test() ->
   % From scuttlebutt protocol guide
   NetworkIdentifier = <<(erlang:list_to_integer("d4a1cb88a66f02f8db635ce26441cc5dac1b08420ceaac230839b755845a9ffb", 16)):256>>,
 
-  ClientEphemeral = libsodium_crypto_sign_ed25519:keypair(),
-  io:format("~p~n", [ClientEphemeral]),
-  ServerEphemeral = libsodium_crypto_sign_ed25519:keypair(),
+  {ClientEphemeralPublicKey, ClientEphemeralSecretKey} = curve25519keypair(),
 
-  ClientHello = hello(ClientEphemeral, NetworkIdentifier),
-  {ok, ClientEphemeralPK} = verify_hello(ClientHello, NetworkIdentifier),
-  io:format("~p~n", [ClientEphemeralPK]),
+  {ServerEphemeralPublicKey, ServerEphemeralSecretKey} = curve25519keypair(),
 
-  ServerHello = hello(ServerEphemeral, NetworkIdentifier),
-  {ok, ServerEphemeralPK} = verify_hello(ServerHello, NetworkIdentifier),
+  ClientHello = hello(ClientEphemeralPublicKey, NetworkIdentifier),
+  {ok, ClientEphemeralPublicKey} = verify_hello(ClientHello, NetworkIdentifier),
+  % io:format("~p~n", [ClientEphemeralPK]),
 
-  ?assertEqual(shared_ab(ClientEphemeralPK, ServerEphemeral), shared_ab(ServerEphemeralPK, ClientEphemeral)),
+  ServerHello = hello(ServerEphemeralPublicKey, NetworkIdentifier),
+  {ok, ServerEphemeralPublicKey} = verify_hello(ServerHello, NetworkIdentifier),
+
+  {ServerLongTermPublicKey, ServerLongTermSecretKey} = libsodium_crypto_sign_ed25519:keypair(),
+
+  Share_aB = libsodium_crypto_scalarmult:crypto_scalarmult(ClientEphemeralSecretKey, libsodium_crypto_sign_ed25519:pk_to_curve25519(ServerLongTermPublicKey)),
+  Share_aB = libsodium_crypto_scalarmult:crypto_scalarmult(libsodium_crypto_sign_ed25519:sk_to_curve25519(ServerLongTermSecretKey), ClientEphemeralPublicKey),
+
+  ?assertEqual(Share_aB, Share_aB),
+
+  Share_ab = libsodium_crypto_scalarmult:crypto_scalarmult(ClientEphemeralSecretKey, ServerEphemeralPublicKey),
+  Share_ab = libsodium_crypto_scalarmult:crypto_scalarmult(ServerEphemeralSecretKey, ClientEphemeralPublicKey),
+
+
+  % libsodium_crypto_scalarmult:crypto_scalarmult(LHS, N).
+
+  % ?assertEqual(shared_ab(ClientEphemeralPK, ServerEphemeral), shared_ab(ServerEphemeralPK, ClientEphemeral)),
 
 
   ?assertEqual(flunk, test).
